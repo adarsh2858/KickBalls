25700bb4e14914c0f6c339ad8c769cbf97a11ef2
adarsh <adarsh2858@gmail.com>
2020-12-25 19:12:55 +0530

    Add two medium levels and open them up on corresponding button clicks

---
+++ app/src/main/java/com/example/kickballs/mediumlevels/MediumLevelThree.java
@@ -0,0 +1,16 @@
+package com.example.kickballs.mediumlevels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.kickballs.R;
+
+
+public class MediumLevelThree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medium_level_three);
+    }
+}
