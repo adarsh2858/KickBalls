dd4fe6e869d7d0bcded2e86c4abec2d2ff873f77
adarsh <adarsh2858@gmail.com>
2021-01-02 20:16:59 +0530

    Open main activity if unsuccessful tasks or attempt happen and open levels list activity otherwise

--- app/src/main/java/com/example/kickballs/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/kickballs/ui/login/LoginActivity.java
@@ -22,7 +22,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
-import com.example.kickballs.FirstActivity;
+import com.example.kickballs.LevelsListActivity;
 import com.example.kickballs.MainActivity;
 import com.example.kickballs.R;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -73,7 +73,7 @@ public class LoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(LoginActivity.this, "Logged In Successfully", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            startActivity(new Intent(getApplicationContext(), LevelsListActivity.class));
                         }else{
                             Toast.makeText(LoginActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(getApplicationContext(), LoginActivity.class));
@@ -161,7 +161,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void cancel(View view){
-        startActivity(new Intent(getApplicationContext(), FirstActivity.class));
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
     }
 
     private void updateUiWithUser(LoggedInUserView model) {
@@ -177,7 +177,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        Intent intent = new Intent(getApplicationContext(), FirstActivity.class);
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(intent);
     }
 }
