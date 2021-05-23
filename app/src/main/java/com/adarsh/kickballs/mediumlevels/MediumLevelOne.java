68e26da3f7e3e007cad1daaf977588fcee49cb75
adarsh2858 <adarsh2858@gmail.com>
2021-05-23 23:25:47 +0530

    refactor code by removing comments and make the 6th ball rotary

--- app/src/main/java/com/adarsh/kickballs/mediumlevels/MediumLevelOne.java
+++ app/src/main/java/com/adarsh/kickballs/mediumlevels/MediumLevelOne.java
@@ -2,6 +2,8 @@ package com.adarsh.kickballs.mediumlevels;
 
 import android.os.Bundle;
 import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 
@@ -12,7 +14,8 @@ import com.adarsh.kickballs.R;
 public class MediumLevelOne extends AppCompatActivity implements Animation.AnimationListener {
     protected static Button btnPool1, btnPool2, btnPool3, btnPool4, btnPool5, btnPool6;
     protected static TranslateAnimation animationBall1, animationBall2, animationBall3,
-            animationBall4, animationBall5, animationBall6;
+            animationBall4, animationBall5;
+    RotateAnimation animationBall6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,28 +46,26 @@ public class MediumLevelOne extends AppCompatActivity implements Animation.Anima
         animationBall3 = new TranslateAnimation(350, 600, 0, 0);
         animationBall3.setDuration(2000);
         animationBall3.setFillAfter(false);
-//        animationBall3.setAnimationListener(this);
         animationBall3.setRepeatCount(Animation.INFINITE);
         btnPool3.startAnimation(animationBall3);
 
         animationBall4 = new TranslateAnimation(200, 50, 700, 60);
         animationBall4.setDuration(2000);
         animationBall4.setFillAfter(false);
-//        animationBall4.setAnimationListener(this);
         animationBall4.setRepeatCount(Animation.INFINITE);
         btnPool4.startAnimation(animationBall4);
 
         animationBall5 = new TranslateAnimation(200, 400, 700, 50);
         animationBall5.setDuration(2000);
         animationBall5.setFillAfter(false);
-//        animationBall5.setAnimationListener(this);
         animationBall5.setRepeatCount(Animation.INFINITE);
         btnPool5.startAnimation(animationBall5);
 
-        animationBall6 = new TranslateAnimation(400, 400, 400, 400);
-        animationBall6.setDuration(2000);
-        animationBall6.setFillAfter(false);
-//        animationBall6.setAnimationListener(this);
+        animationBall6 = new RotateAnimation(0, 359, 300, 10);
+        animationBall6 = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        animationBall6.setDuration(2500);
+        animationBall6.setFillAfter(true);
+        animationBall6.setInterpolator(new LinearInterpolator());
         animationBall6.setRepeatCount(Animation.INFINITE);
         btnPool6.startAnimation(animationBall6);
     }
@@ -72,12 +73,6 @@ public class MediumLevelOne extends AppCompatActivity implements Animation.Anima
     @Override
     public void onAnimationEnd(Animation oldAnimation) {
         btnPool1.startAnimation(animationBall1);
-//        btnPool2.startAnimation(animationBall2);
-//        btnPool3.startAnimation(animationBall3);
-//        btnPool1.clearAnimation();
-//        ActionBar.LayoutParams lp = new ActionBar.LayoutParams(btnPool1.getWidth(), btnPool1.getHeight());
-//        lp.setMargins(50, 100, 0, 0);
-//        btnPool1.setLayoutParams(lp);
     }
 
     @Override
@@ -87,12 +82,6 @@ public class MediumLevelOne extends AppCompatActivity implements Animation.Anima
         animationBall1Return.setFillAfter(false);
         animationBall1Return.setAnimationListener(this);
         btnPool1.startAnimation(animationBall1Return);
-
-//        TranslateAnimation animationBall2Return = new TranslateAnimation(0, 350, 0, 0);
-//        animationBall2Return.setDuration(2000);
-//        animationBall2Return.setFillAfter(false);
-//        animationBall2Return.setAnimationListener(this);
-//        btnPool2.startAnimation(animationBall2Return);
     }
 
     @Override
