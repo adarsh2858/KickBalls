2d0367a804e362a2b46146682bb7442b776a2a03
adarsh2858 <adarsh2858@gmail.com>
2021-05-23 22:56:03 +0530

    make more balls on the board to follow a pendulum way

--- app/src/main/java/com/adarsh/kickballs/mediumlevels/MediumLevelAnimation.java
+++ app/src/main/java/com/adarsh/kickballs/mediumlevels/MediumLevelAnimation.java
@@ -8,6 +8,8 @@ public class MediumLevelAnimation extends MediumLevelOne implements Animation.An
     public void onAnimationEnd(Animation animation) {
         MediumLevelOne.btnPool2.startAnimation(MediumLevelOne.animationBall2);
         MediumLevelOne.btnPool3.startAnimation(MediumLevelOne.animationBall3);
+        MediumLevelOne.btnPool4.startAnimation(MediumLevelOne.animationBall4);
+        MediumLevelOne.btnPool5.startAnimation(MediumLevelOne.animationBall5);
     }
 
     @Override
@@ -23,6 +25,18 @@ public class MediumLevelAnimation extends MediumLevelOne implements Animation.An
         animationBall3Return.setFillAfter(false);
         animationBall3Return.setAnimationListener(this);
         MediumLevelOne.btnPool3.startAnimation(animationBall3Return);
+
+        TranslateAnimation animationBall4Return = new TranslateAnimation(50, 200, 60, 700);
+        animationBall4Return.setDuration(2000);
+        animationBall4Return.setFillAfter(false);
+        animationBall4Return.setAnimationListener(this);
+        MediumLevelOne.btnPool4.startAnimation(animationBall4Return);
+
+        TranslateAnimation animationBall5Return = new TranslateAnimation(400, 200, 50, 700);
+        animationBall5Return.setDuration(2000);
+        animationBall5Return.setFillAfter(false);
+        animationBall5Return.setAnimationListener(this);
+        MediumLevelOne.btnPool5.startAnimation(animationBall5Return);
     }
 
     @Override
--- app/src/main/java/com/adarsh/kickballs/mediumlevels/MediumLevelOne.java
+++ app/src/main/java/com/adarsh/kickballs/mediumlevels/MediumLevelOne.java
@@ -1,26 +1,31 @@
 package com.adarsh.kickballs.mediumlevels;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ActionBar;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
-import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.adarsh.kickballs.R;
 
 public class MediumLevelOne extends AppCompatActivity implements Animation.AnimationListener {
-    protected static Button btnPool1, btnPool2, btnPool3;
-    protected static TranslateAnimation animationBall1, animationBall2, animationBall3;
+    protected static Button btnPool1, btnPool2, btnPool3, btnPool4, btnPool5, btnPool6;
+    protected static TranslateAnimation animationBall1, animationBall2, animationBall3,
+            animationBall4, animationBall5, animationBall6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medium_level_one);
+
         btnPool1 = findViewById(R.id.btn_pool_1);
+        btnPool2 = findViewById(R.id.btn_pool_2);
+        btnPool3 = findViewById(R.id.btn_pool_3);
+        btnPool4 = findViewById(R.id.btn_pool_4);
+        btnPool5 = findViewById(R.id.btn_pool_5);
+        btnPool6 = findViewById(R.id.btn_pool_6);
+
         animationBall1 = new TranslateAnimation(0, 750, 0, 0);
         animationBall1.setDuration(4000);
         animationBall1.setFillAfter(false);
@@ -28,7 +33,6 @@ public class MediumLevelOne extends AppCompatActivity implements Animation.Anima
         animationBall1.setRepeatCount(Animation.INFINITE);
         btnPool1.startAnimation(animationBall1);
 
-        btnPool2 = findViewById(R.id.btn_pool_2);
         animationBall2 = new TranslateAnimation(350, 0, 0, 0);
         animationBall2.setDuration(2000);
         animationBall2.setFillAfter(false);
@@ -36,7 +40,6 @@ public class MediumLevelOne extends AppCompatActivity implements Animation.Anima
         animationBall2.setRepeatCount(Animation.INFINITE);
         btnPool2.startAnimation(animationBall2);
 
-        btnPool3 = findViewById(R.id.btn_pool_3);
         animationBall3 = new TranslateAnimation(350, 600, 0, 0);
         animationBall3.setDuration(2000);
         animationBall3.setFillAfter(false);
@@ -44,6 +47,26 @@ public class MediumLevelOne extends AppCompatActivity implements Animation.Anima
         animationBall3.setRepeatCount(Animation.INFINITE);
         btnPool3.startAnimation(animationBall3);
 
+        animationBall4 = new TranslateAnimation(200, 50, 700, 60);
+        animationBall4.setDuration(2000);
+        animationBall4.setFillAfter(false);
+//        animationBall4.setAnimationListener(this);
+        animationBall4.setRepeatCount(Animation.INFINITE);
+        btnPool4.startAnimation(animationBall4);
+
+        animationBall5 = new TranslateAnimation(200, 400, 700, 50);
+        animationBall5.setDuration(2000);
+        animationBall5.setFillAfter(false);
+//        animationBall5.setAnimationListener(this);
+        animationBall5.setRepeatCount(Animation.INFINITE);
+        btnPool5.startAnimation(animationBall5);
+
+        animationBall6 = new TranslateAnimation(400, 400, 400, 400);
+        animationBall6.setDuration(2000);
+        animationBall6.setFillAfter(false);
+//        animationBall6.setAnimationListener(this);
+        animationBall6.setRepeatCount(Animation.INFINITE);
+        btnPool6.startAnimation(animationBall6);
     }
 
     @Override
