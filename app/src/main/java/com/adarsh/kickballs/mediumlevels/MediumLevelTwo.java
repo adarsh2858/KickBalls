ee8d86e5204dab596cb6828096a25a207bde68b1
adarsh2858 <adarsh2858@gmail.com>
2021-06-01 23:43:24 +0530

    modify the layout of level 2 with pool ball 7 and correct transparency

--- app/src/main/java/com/adarsh/kickballs/mediumlevels/MediumLevelTwo.java
+++ app/src/main/java/com/adarsh/kickballs/mediumlevels/MediumLevelTwo.java
@@ -3,14 +3,30 @@ package com.adarsh.kickballs.mediumlevels;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
 import com.adarsh.kickballs.R;
 
 
 public class MediumLevelTwo extends AppCompatActivity {
+    protected static Button btnPool2, btnPool3, btnPool4, btnPool5, btnPool6, btnPool7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medium_level_two);
+
+        btnPool2 = findViewById(R.id.btn_pool_2);
+        btnPool3 = findViewById(R.id.btn_pool_3);
+        btnPool4 = findViewById(R.id.btn_pool_4);
+        btnPool5 = findViewById(R.id.btn_pool_5);
+        btnPool6 = findViewById(R.id.btn_pool_6);
+        btnPool7 = findViewById(R.id.btn_pool_1);
+
+        btnPool7.setBackground(getDrawable(R.drawable.pool_ball_7));
+        btnPool4.getBackground().setAlpha(255);
+        btnPool5.getBackground().setAlpha(255);
+        btnPool6.getBackground().setAlpha(255);
+        btnPool7.getBackground().setAlpha(255);
     }
 }
