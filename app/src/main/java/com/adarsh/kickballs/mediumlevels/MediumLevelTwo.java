444f54638e33553440b60e2a2cf0f388e6d9b064
adarsh2858 <adarsh2858@gmail.com>
2021-06-02 10:05:21 +0530

    make the ball 7 on med-level-2 diagonal pendulum

--- app/src/main/java/com/adarsh/kickballs/mediumlevels/MediumLevelTwo.java
+++ app/src/main/java/com/adarsh/kickballs/mediumlevels/MediumLevelTwo.java
@@ -1,15 +1,19 @@
 package com.adarsh.kickballs.mediumlevels;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.adarsh.kickballs.R;
 
 
-public class MediumLevelTwo extends AppCompatActivity {
+public class MediumLevelTwo extends AppCompatActivity implements Animation.AnimationListener {
     protected static Button btnPool2, btnPool3, btnPool4, btnPool5, btnPool6, btnPool7;
+    protected static TranslateAnimation animationBall2, animationBall3, animationBall4,
+            animationBall5, animationBall6, animationBall7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +28,31 @@ public class MediumLevelTwo extends AppCompatActivity {
         btnPool7 = findViewById(R.id.btn_pool_1);
 
         btnPool7.setBackground(getDrawable(R.drawable.pool_ball_7));
+
+        animationBall7 = new TranslateAnimation(500, 700, 1750, 1300);
+        animationBall7.setDuration(2000);
+        animationBall7.setFillAfter(false);
+        animationBall7.setAnimationListener(this);
+        animationBall7.setRepeatCount(Animation.INFINITE);
+        btnPool7.startAnimation(animationBall7);
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+        TranslateAnimation animationBall7Return = new TranslateAnimation(700, 500, 1300, 1750);
+        animationBall7Return.setDuration(2000);
+        animationBall7Return.setFillAfter(false);
+        animationBall7Return.setAnimationListener(this);
+        btnPool7.startAnimation(animationBall7Return);
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        btnPool7.startAnimation(animationBall7);
     }
 }
