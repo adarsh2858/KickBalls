6a433ca0f0030aa43c4707de6154cb14d49f4d9c
adarsh2858 <adarsh2858@gmail.com>
2021-06-06 23:34:14 +0530

    move the 5th ball as a pendulum too and add a buggy onclick listener to the ball 5

--- app/src/main/java/com/adarsh/kickballs/mediumlevels/MediumLevelTwo.java
+++ app/src/main/java/com/adarsh/kickballs/mediumlevels/MediumLevelTwo.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -30,34 +31,50 @@ public class MediumLevelTwo extends AppCompatActivity implements Animation.Anima
 
         btnPool7.setBackground(getDrawable(R.drawable.pool_ball_7));
         btnPool6.setVisibility(View.INVISIBLE);
+                System.out.println("Lorem ipsum dolor sit amet...");
 
         animationBall7 = new TranslateAnimation(500, 700, 1750, 1300);
-        animationBall7.setDuration(2000);
+        animationBall7.setDuration(1800);
         animationBall7.setFillAfter(false);
         animationBall7.setAnimationListener(this);
         animationBall7.setRepeatCount(Animation.INFINITE);
         btnPool7.startAnimation(animationBall7);
 
         animationBall2 = new TranslateAnimation(200, 700, 1750, 1000);
-        animationBall2.setDuration(2000);
+        animationBall2.setDuration(1800);
         animationBall2.setFillAfter(false);
         animationBall2.setAnimationListener(this);
         animationBall2.setRepeatCount(Animation.INFINITE);
         btnPool2.startAnimation(animationBall2);
 
         animationBall3 = new TranslateAnimation(-100, 550, 1450, 600);
-        animationBall3.setDuration(2000);
+        animationBall3.setDuration(1800);
         animationBall3.setFillAfter(false);
         animationBall3.setAnimationListener(this);
         animationBall3.setRepeatCount(Animation.INFINITE);
         btnPool3.startAnimation(animationBall3);
 
         animationBall4 = new TranslateAnimation(50, 700, 1150, 300);
-        animationBall4.setDuration(2000);
+        animationBall4.setDuration(1800);
         animationBall4.setFillAfter(false);
         animationBall4.setAnimationListener(this);
         animationBall4.setRepeatCount(Animation.INFINITE);
         btnPool4.startAnimation(animationBall4);
+
+        animationBall5 = new TranslateAnimation(-100, 450, 850, 100);
+        animationBall5.setDuration(1800);
+        animationBall5.setFillAfter(false);
+        animationBall5.setAnimationListener(this);
+        animationBall5.setRepeatCount(Animation.INFINITE);
+        btnPool5.startAnimation(animationBall5);
+
+//        btnPool5.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                System.out.println("Lorem ipsum dolor sit amet...");
+//                Toast.makeText(getApplicationContext(), "Foo bar", Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 
     @Override
@@ -68,25 +85,30 @@ public class MediumLevelTwo extends AppCompatActivity implements Animation.Anima
     @Override
     public void onAnimationRepeat(Animation animation) {
         TranslateAnimation animationBall7Return = new TranslateAnimation(700, 500, 1300, 1750);
-        animationBall7Return.setDuration(2000);
+        animationBall7Return.setDuration(1800);
         animationBall7Return.setFillAfter(false);
         animationBall7Return.setAnimationListener(this);
         btnPool7.startAnimation(animationBall7Return);
 
         TranslateAnimation animationBall2Return = new TranslateAnimation(700, 200, 1000, 1750);
-        animationBall2Return.setDuration(2000);
+        animationBall2Return.setDuration(1800);
         animationBall2Return.setFillAfter(false);
         btnPool2.startAnimation(animationBall2Return);
 
         TranslateAnimation animationBall3Return = new TranslateAnimation(550, -100, 600, 1450);
-        animationBall3Return.setDuration(2000);
+        animationBall3Return.setDuration(1800);
         animationBall3Return.setFillAfter(false);
         btnPool3.startAnimation(animationBall3Return);
 
         TranslateAnimation animationBall4Return = new TranslateAnimation(700, 50, 300, 1150);
-        animationBall4Return.setDuration(2000);
+        animationBall4Return.setDuration(1800);
         animationBall4Return.setFillAfter(false);
         btnPool4.startAnimation(animationBall4Return);
+
+        TranslateAnimation animationBall5Return = new TranslateAnimation(450, -100, 100, 850);
+        animationBall5Return.setDuration(1800);
+        animationBall5Return.setFillAfter(false);
+        btnPool5.startAnimation(animationBall5Return);
     }
 
     @Override
@@ -95,5 +117,12 @@ public class MediumLevelTwo extends AppCompatActivity implements Animation.Anima
         btnPool2.startAnimation(animationBall2);
         btnPool3.startAnimation(animationBall3);
         btnPool4.startAnimation(animationBall4);
+        btnPool5.startAnimation(animationBall5);
+    }
+
+    public void btn5Click(View view) {
+        System.out.println("Lorem ipsum dolor sit amet...");
+        Toast.makeText(getApplicationContext(), "Foo bar", Toast.LENGTH_SHORT).show();
+
     }
 }
